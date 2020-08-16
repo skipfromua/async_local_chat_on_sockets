c7695e7e7bd1548c9d9942ea9852ac257baf4ddd
Oleksandr <skipfromua@gmail.com>
2020-08-16 19:05:50 +0300

    server_works_fine_almost

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 5000))
+sock.connect(('127.0.0.1', 5004))
 
-while True:
-    message = input().encode()
-    sock.send(message)
-    result = sock.recv(4096)
-    print(result.decode())
+message = input().encode()
+sock.send(message)
+result = sock.recv(4096)
+print(result.decode())
--- server.py
+++ server.py
@@ -1,15 +1,44 @@
-import socketserver
+import socket
+import asyncio
+from time import sleep
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 5004))
+sock.listen()
+sock.setblocking(False)
+
+clients = []
 
 
-class AllClientsRecieveHandler(socketserver.BaseRequestHandler):
-    clients = []
+async def check_new_connections():
+    while True:
+        try:
+            client, addr = sock.accept()
+            client.setblocking(False)
+            clients.append((client, addr))
+        except socket.error:
+            await asyncio.sleep(0)
 
-    def handle(self) -> None:
-        data = self.request.recv(4096).strip()
-        print(self.client_address)
-        self.request.sendall(data)
+
+async def check_new_requests():
+    while True:
+        if not clients:
+            await asyncio.sleep(0)
+        for client, addr in clients:
+            try:
+                result = client.recv(4096)
+                client_addr = addr
+            except socket.error:
+                print('have clients')
+                await asyncio.sleep(0)
+                continue
+            for answer_for_client, addr in clients:
+                if client_addr != addr:
+                    answer_for_client.send(result)
 
 
-if __name__ == '__main__':
-    with socketserver.TCPServer(('127.0.0.1', 5000), AllClientsRecieveHandler) as server:
-        server.serve_forever()
+ioloop = asyncio.get_event_loop()
+tasks = [ioloop.create_task(check_new_connections()), ioloop.create_task(check_new_requests())]
+wait_tasks = asyncio.wait(tasks)
+ioloop.run_until_complete(wait_tasks)
+ioloop.close()
