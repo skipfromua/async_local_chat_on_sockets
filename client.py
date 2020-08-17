11b7b1190a83eb372e5ca953af3e7a9bc03d01e5
Oleksandr <skipfromua@gmail.com>
2020-08-17 10:40:26 +0300

    client_safe_leave

--- client.py
+++ client.py
@@ -8,20 +8,25 @@ sock.connect((sys.argv[1], int(sys.argv[2])))
 
 
 def send_message():
+    print('You are connected to chat')
+    print('If you want to quit send "\quit"')
     while True:
         message = input().encode()
+        if message.decode() == "\quit":
+            sock.send(b'Anonymous user left the chat')
+            sock.close()
+            break
         sock.send(message)
 
 
-
 def recieve_message():
-    while True:
+    while t1.is_alive():
         result = sock.recv(4096)
         print(result.decode())
 
 
 t1 = Thread(target=send_message)
-t2 = Thread(target=recieve_message)
+t2 = Thread(target=recieve_message, daemon=True)
 
 t1.start()
 t2.start()
